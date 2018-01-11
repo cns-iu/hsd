750e78364d543c762eda3be55c3a10e979674794
Daniel Bolin <axbolin@iu.edu>
2018-01-11 10:31:11 -0500

    Setup core and shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './hsd-ui/material.module';
+import { CoreModule } from './core';
+import { SharedModule } from './shared';
 import { HsdUiModule } from './hsd-ui';
 import { AppComponent } from './app.component';
 
@@ -13,6 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     MaterialModule,
+    CoreModule,
+    SharedModule,
     HsdUiModule
   ],
   providers: [],
