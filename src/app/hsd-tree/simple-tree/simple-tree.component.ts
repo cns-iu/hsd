4e82f32bbacfc140725b678ae24ce527bd83b598
Gagandeep Singh <whg_27@yahoo.com>
2018-01-11 14:37:28 -0500

    coloring of internal nodes based on their type on simple-tree

--- src/app/hsd-tree/simple-tree/simple-tree.component.ts
+++ src/app/hsd-tree/simple-tree/simple-tree.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import * as vega from 'vega';
+import * as data from './spec.json';
 
 @Component({
   selector: 'hsd-simple-tree',
@@ -8,28 +9,30 @@ import * as vega from 'vega';
 })
 export class SimpleTreeComponent implements OnInit {
   private parentNativeElement: any;
-  private spec = `https://gist.githubusercontent.com/axdanbol/
-d77ffabcc9cfb2f3c9b7d7e2404a7f8f/raw/
-69641dc469f9aff598523c61a203965dcd666ed3/hsd_vega_spec.json`;
 
   constructor(element: ElementRef) {
     this.parentNativeElement = element.nativeElement; // to get native parent element of this component
   }
 
   ngOnInit() {
-    vega.loader()
-      .load(this.spec)
-      .then((data) => {
-        this.render(JSON.parse(data));
-      });
+  this.render();
   }
 
-  render(spec) {
-    const view = new vega.View(vega.parse(spec))
+  render() {
+    const view = new vega.View(vega.parse(data))
       .renderer('canvas')  // set renderer (canvas or svg)
       .initialize(this.parentNativeElement) // initialize view within parent DOM container
       .hover()             // enable hover encode set processing
       .run();
+    // for exporting to png
+    //   view.toImageURL('png').then((url) => {
+    //   const link = document.createElement('a');
+    //   link.setAttribute('href', url);
+    //   link.setAttribute('target', '_blank');
+    //   link.setAttribute('download', 'vega-export.png');
+    //   link.dispatchEvent(new MouseEvent('click'));
+    // }).catch((error) => { /* error handling */ });
+
   }
 
 }
