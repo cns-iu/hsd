dd3a9795e5906aa5d962e628e15e7b4ad73fd4bd
Bruce Herr II <bherr@indiana.edu>
2018-01-12 16:01:42 -0500

    Minor style fix Added a note for building releases

--- build-release.sh
+++ build-release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Assumes the gh-pages branch has been checked out to ../hsd-release/
+# Use this command:
+# > git clone https://github.com/cns-iu/hsd.git -b gh-pages ../hsd-release/
+
 ng build --prod --aot --output-hashing=none --sourcemaps --base-href /hsd/
 
 cp -r dist/* ../hsd-release/
