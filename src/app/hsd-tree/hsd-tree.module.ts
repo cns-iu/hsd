9900bd3b7ea61ccd93656872767ed294b0b430a6
axdanbol <daniel_bolin@yahoo.com>
2018-02-19 10:32:39 -0500

    Created v3 tree + adjusted text over bars

--- src/app/hsd-tree/hsd-tree.module.ts
+++ src/app/hsd-tree/hsd-tree.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import { SimpleTreeComponent } from './simple-tree/simple-tree.component';
 import { SumTreeComponent } from './sum-tree/sum-tree.component';
 import { SumTreeV2Component } from './sum-tree-v2/sum-tree-v2.component';
+import { SumTreeV3Component } from './sum-tree-v3/sum-tree-v3.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [
-    SimpleTreeComponent, SumTreeComponent, SumTreeV2Component
+    SimpleTreeComponent, SumTreeComponent, SumTreeV2Component,
+    SumTreeV3Component
   ],
-  declarations: [SimpleTreeComponent, SumTreeComponent, SumTreeV2Component]
+  declarations: [
+    SimpleTreeComponent, SumTreeComponent, SumTreeV2Component,
+    SumTreeV3Component
+  ]
 })
 export class HsdTreeModule { }
---
+++ src/app/hsd-tree/sum-tree-v3/spec/signals/index.ts
@@ -0,0 +1,6 @@
+import * as nodeClicked from './node-clicked.json';
+import * as boxClicked from './box-clicked.json';
+
+export default [].concat(
+  nodeClicked, boxClicked
+);
