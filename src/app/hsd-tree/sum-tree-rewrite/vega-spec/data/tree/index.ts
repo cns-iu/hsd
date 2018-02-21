9e4616370d13a1acd0251e4036fb4eeca8dbcb30
axdanbol <daniel_bolin@yahoo.com>
2018-02-21 12:53:55 -0500

    Datasets to split nodes after tree layout

--- src/app/hsd-tree/sum-tree-rewrite/vega-spec/data/tree/index.ts
+++ src/app/hsd-tree/sum-tree-rewrite/vega-spec/data/tree/index.ts
@@ -1,6 +1,10 @@
 import * as treeDataSet from './tree.json';
 import * as treeLinksDataSet from './links.json';
 
+import * as singleNodesDataSet from './single-nodes.json';
+import * as summaryNodesDataSet from './summary-nodes.json';
+
 export default [].concat(
-  treeDataSet, treeLinksDataSet
+  treeDataSet, treeLinksDataSet,
+  singleNodesDataSet, summaryNodesDataSet
 );
