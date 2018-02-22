782488c8f21c5a29d4b71133ee9f392d34af674f
axdanbol <daniel_bolin@yahoo.com>
2018-02-22 13:24:27 -0500

    Added intermediate dataset for summary symbols

--- src/app/hsd-tree/sum-tree-rewrite/vega-spec/marks/tree/summary-nodes/symbol-group/index.ts
+++ src/app/hsd-tree/sum-tree-rewrite/vega-spec/marks/tree/summary-nodes/symbol-group/index.ts
@@ -1,8 +1,11 @@
 import * as group from './group.json';
+import * as breakdownDataSet from './breakdown.json';
 import * as symbolMarks from './symbol.json';
 
 export default [Object.assign({}, group, {
-  data: (group['data'] || []).concat(),
+  data: (group['data'] || []).concat(
+    breakdownDataSet
+  ),
   marks: (group['marks'] || []).concat(
     symbolMarks
   )
